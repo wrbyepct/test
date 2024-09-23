33ca69c0402469ee7e9695012d11bef8f6487061
Furthermore <wrbyepcr11@gmail.com>
2024-09-23 17:47:17 +0800

    rewrite d080120 histroy

--- main.py
+++ main.py
@@ -2,10 +2,6 @@
 def test_func():
 
     b = "YEAH!! this is indeed very COOL!"
-
     d = "NAH this is extra not COOL!"
-
-    e = "I'm happy with what i have"
-
-    return f"cool funciton {d}{b}{e}"
+    return f"cool funciton {b}"
 
