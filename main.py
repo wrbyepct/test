083d5d5947fb245be7c18dcc2949af0938aee1bb
Furthermore <wrbyepcr11@gmail.com>
2024-09-23 23:18:17 +0800

    add an f

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ def test_func():
 
     b = "YEAH!! this is indeed very COOL!"
     d = "NAH this is extra not COOL!" + "I added another line to rewrite history"
-    return f"cool funciton {b}"
+
+    e = "I'm happy with what i have"
+    
+    f = "add an f"
+
+    return f"cool funciton {d}{b}{e}"
 
